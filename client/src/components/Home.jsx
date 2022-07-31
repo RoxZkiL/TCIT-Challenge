47ce884f1f31479a358b0b9a3a2c64ee102a5e11
Jesus Matute <mttrox@gmail.com>
2022-07-31 18:32:43 -0400

    Fix

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import TableList from "./TableList";
 import SearchPost from "./SearchPost";
 import PostForm from "./PostForm";
-import "../styles/container.css";
 
 const Home = () => {
   return (
